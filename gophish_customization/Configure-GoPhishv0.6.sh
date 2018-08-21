059eadcb7e2cea4dffc9d55f5e9fcfb6bfcaefd1
Figgs1 <frank@fadepaola.com>
2018-08-21 08:57:01 -0400

    Update Configure-GoPhishv0.6.sh

--- gophish_customization/Configure-GoPhishv0.6.sh
+++ gophish_customization/Configure-GoPhishv0.6.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo yum install postfix -y
-sudo mkdir /usr/share/gophish-v0.6.0/
-sudo mkdir /var/log/gophish-v0.6.0/
-sudo aws s3 cp --recursive s3://software-share-913373464384/gophish-v0.6.0-linux-64bit/ /usr/share/gophish-v0.6.0/
+yum install postfix -y
+mkdir /usr/share/gophish-v0.6.0/
+mkdir /var/log/gophish-v0.6.0/
+aws s3 cp --recursive s3://software-share-913373464384/gophish-v0.6.0-linux-64bit/ /usr/share/gophish-v0.6.0/
 chmod +x /usr/share/gophish-v0.6.0/gophish
-sudo cp /usr/share/gophish-v0.6.0/scripts/gophish  /etc/init.d/
-sudo chmod +x /etc/init.d/gophish
-sudo chkconfig --add gophish
-sudo chkconfig --levels 2345 gophish on
+cp /usr/share/gophish-v0.6.0/scripts/gophish  /etc/init.d/
+chmod +x /etc/init.d/gophish
+chkconfig --add gophish
+chkconfig --levels 2345 gophish on
 #edit main.cf file and change line #mynetworks_style = host to mynetworks_style = host
-sudo sed -i "s/^#mynetworks_style = host/mynetworks_style = host/g" /etc/postfix/main.cf
-sudo sed -i "s/^#relayhost = $mydomain/relayhost = $mydomain/g" /etc/postfix/main.cf
-sudo reboot now
+sed -i "s/^#mynetworks_style = host/mynetworks_style = host/g" /etc/postfix/main.cf
+sed -i "s/^#relayhost = $mydomain/relayhost = $mydomain/g" /etc/postfix/main.cf
+reboot now
